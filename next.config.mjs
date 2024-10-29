5bd82fe25e25397ff4f3eba1fb64639163d2086a
jehadurre <emran.jehadur@gmail.com>
2024-10-30 04:04:06 +0600

    #fix issues env

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/jehadurre.me',
-    trailingSlash: true,  // Ensures generated paths are compatible with static hosting
-    env: {
-        NEXT_PUBLIC_BASE_PATH: '/jehadurre.me',  // This makes it accessible in components
-    },
-};
+const nextConfig = {   };
 
 export default nextConfig;
