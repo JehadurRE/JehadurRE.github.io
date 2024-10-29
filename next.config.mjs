c913c33b4864f30fd768358968e56142b488291c
jehadurre <emran.jehadur@gmail.com>
2024-10-30 03:19:18 +0600

    #add export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:'export',
+    output: 'export',
     basePath: '/jehadurre.me',
+    trailingSlash: true,  // Ensures generated paths are compatible with static hosting
 };
 
 export default nextConfig;
